a7bd0c18dba9ed96caeea9100681ec857954647f
José Pedro Belo <josepedroribeirobelo@gmail.com>
2024-07-25 16:05:51 -0300

    Adicionando variáveis de configuração para gerar máscaras binárias

--- config/config.py
+++ config/config.py
@@ -1,4 +1,10 @@
 # VARIÁVEIS DE CONFIGURAÇÂO
 
+
 #Tamanho dos subtiles utilizados em divide_orthomosaic
 SUBTILES_SIZE = (256,256)
+
+#Limiar para binarizar máscara de segmentação
+BINARIZE_THRESHOLD = 128    
+#Valor do pixel da classe segunda classe binária (vegetação)
+BINARY_MAX = 255
