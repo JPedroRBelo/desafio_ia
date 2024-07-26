9f3048c73098a22a16e391029d37ddb3d37bc75f
José Pedro Belo <josepedroribeirobelo@gmail.com>
2024-07-26 13:37:04 -0300

    Etapa 3: códigos para treinamento de modelo de segmentação (DeepLabV3+_Resnet50) para vegetação.

---
+++ net/resnet50.py
@@ -0,0 +1,205 @@
+import os
+import logging
+from PIL import Image
+import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import Tuple, List, Optional
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Input, Conv2D, UpSampling2D, Concatenate
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import BinaryIoU
+
+class SegmentationResnet50:
+    """
+    Classe para treinar o modelo DeepLabV3+ com ResNet50 para segmentação semântica de áreas vegetadas.
+    """
+    def __init__(self, img_width: int = 256, img_height: int = 256, batch_size: int = 8, epochs: int = 100, validation_split: float = 0.1, test_split: float = 0, model_path: Optional[str] = None):
+        self.img_width = img_width
+        self.img_height = img_height
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.validation_split = validation_split
+        self.test_split = test_split
+        self.model = self.create_model(input_size=(self.img_height, self.img_width, 3))
+        if model_path and os.path.exists(model_path):
+            self.model = load_model(model_path, custom_objects={'BinaryIoU': BinaryIoU(threshold=0.5)})
+        else:
+            self.model = self.create_model(input_size=(self.img_height, self.img_width, 3))
+
+    def create_model(self, input_size: Tuple[int, int, int]) -> Model:
+        """
+        Cria o modelo DeepLabV3+ com ResNet50 como backbone.
+
+        Args:
+            input_size (Tuple): Dimensão de entrada da imagem (altura, largura, canais).
+
+        Returns:
+            Model: Modelo compilado.
+        """
+
+        base_model = ResNet50(weights='imagenet', include_top=False, input_shape=input_size)
+        
+        # Extraindo camadas relevantes para o DeepLabV3+
+        x = base_model.output
+
+        # Atrous Spatial Pyramid Pooling (ASPP)
+        # ASPP é um módulo de segmentação semântica para reamostragem de uma dada camada
+        # de feições em múltiplas taxas antes da convolução. 
+        # Isso equivale a sondar a imagem original com múltiplos filtros que têm campos
+        # de visão efetivos complementares, capturando assim objetos, bem como contexto
+        # de imagem útil em múltiplas escalas. Em vez de realmente reamostragem de feições, 
+        # o mapeamento é implementado usando múltiplas camadas convolucionais atrous 
+        # paralelas com diferentes taxas de amostragem.
+
+        b4 = Conv2D(256, (1, 1), padding='same', activation='relu')(x)
+        b4 = Conv2D(256, (3, 3), padding='same', activation='relu', dilation_rate=(6, 6))(b4)  
+        b6 = Conv2D(256, (1, 1), padding='same', activation='relu')(x)
+        b6 = Conv2D(256, (3, 3), padding='same', activation='relu', dilation_rate=(12, 12))(b6)    
+        b8 = Conv2D(256, (1, 1), padding='same', activation='relu')(x)
+        b8 = Conv2D(256, (3, 3), padding='same', activation='relu', dilation_rate=(18, 18))(b8)
+    
+        # Concatenando e aplicando uma convolução final
+        x = Concatenate()([x, b4, b6, b8])
+        x = Conv2D(256, (1, 1), padding='same', activation='relu')(x)
+        
+        # Upsampling para atingir a resolução final
+        x = UpSampling2D(size=(2, 2))(x)
+        x = Conv2D(128, (3, 3), padding='same', activation='relu')(x)
+        x = UpSampling2D(size=(2, 2))(x)
+        x = Conv2D(64, (3, 3), padding='same', activation='relu')(x)
+        x = UpSampling2D(size=(2, 2))(x)
+        x = Conv2D(32, (3, 3), padding='same', activation='relu')(x)
+        x = UpSampling2D(size=(2, 2))(x)
+        x = Conv2D(16, (3, 3), padding='same', activation='relu')(x)
+
+        # UpSampling final
+        x = UpSampling2D(size=(2, 2))(x)  
+        
+        # Camada de saída com a mesma dimensão das máscaras
+        x = Conv2D(1, (1, 1), padding='same', activation='sigmoid')(x)
+        
+        model = Model(inputs=base_model.input, outputs=x)
+        model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=[BinaryIoU(threshold=0.5),'accuracy'])    
+        return model
+    
+    def load_data(self, img_dir: str, mask_dir: str, img_size: Tuple[int, int]) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Carrega e preprocessa as imagens e máscaras usando PIL.
+
+        Args:
+            img_dir (str): Diretório contendo as imagens.
+            mask_dir (str): Diretório contendo as máscaras.
+            img_size (Tuple): Dimensão desejada para as imagens (altura, largura).
+
+        Returns:
+            Tuple: Arrays numpy contendo as imagens e máscaras preprocessadas.
+        """
+        img_files = sorted([f for f in os.listdir(img_dir) if f.endswith('.png')])
+        mask_files = sorted([f for f in os.listdir(mask_dir) if f.endswith('.png')])
+        
+        images = []
+        masks = []
+        
+        for img_file, mask_file in zip(img_files, mask_files):
+            img = Image.open(os.path.join(img_dir, img_file)).convert('RGB').resize((img_size[1], img_size[0]))
+            img = np.array(img) / 255.0  # Normalizar para [0, 1]
+            
+            mask = Image.open(os.path.join(mask_dir, mask_file)).resize((img_size[1], img_size[0]))
+            mask = np.array(mask)
+            mask = np.expand_dims(mask, axis=-1)
+            
+            images.append(img)
+            masks.append(mask)
+        
+        return np.array(images), np.array(masks)
+
+    def plot_results(self,images, masks, predictions, num=5):
+        """
+        Plota os resultados das previsões do modelo.
+
+        Args:
+            images (np.ndarray): Conjunto de imagens originais.
+            masks (np.ndarray): Conjunto de máscaras reais.
+            predictions (np.ndarray): Conjunto de previsões do modelo.
+            num (int): Número de amostras a serem plotadas.
+        """
+        _, axes = plt.subplots(num, 3, figsize=(15, num * 5))
+        for i in range(num):
+            axes[i, 0].imshow(images[i])
+            axes[i, 0].set_title('Imagem Original')
+            axes[i, 0].axis('off')
+            
+            axes[i, 1].imshow(masks[i].squeeze(), cmap='gray')
+            axes[i, 1].set_title('Máscara Real')
+            axes[i, 1].axis('off')
+            
+            axes[i, 2].imshow(predictions[i].squeeze(), cmap='gray')
+            axes[i, 2].set_title('Previsão do Modelo')
+            axes[i, 2].axis('off')
+        
+        plt.tight_layout()
+        plt.show()
+
+    def save_model(self, model_path: str = os.path.join('models','veg_segmentation.h5')) -> None:
+        """
+        Salva o modelo.
+
+        Args:
+            model_path (str): Caminho para salvar o modelo.
+        """
+        self.model.save(model_path)
+
+    def train(self, dataset_dir: str, masks_dir: str) -> Model:
+        """
+        Treina o modelo DeepLabV3+ com ResNet50 usando as imagens e máscaras.
+
+        Args:
+            rgb_path (str): Diretório contendo as imagens RGB.
+            groundtruth_path (str): Diretório contendo as máscaras de verdade-terreno.
+        """
+        # Carregar os dados
+        images, masks = self.load_data(dataset_dir, masks_dir, (self.img_height, self.img_width))
+
+        # Separar o conjunto de treino e teste
+        split_idx = int(len(images) * (1 - self.test_split))
+        train_images, test_images = images[:split_idx], images[split_idx:]
+        train_masks, test_masks = masks[:split_idx], masks[split_idx:]
+
+        # Treinar o modelo
+        self.model.fit(train_images, train_masks, batch_size=self.batch_size, epochs=self.epochs, validation_split=self.validation_split)
+
+        if len(test_images) > 0:
+            # Predições no conjunto de teste
+            predictions = self.model.predict(test_images)
+            # Arredonda as segmentações para garantir que sejam binárias
+            predictions = (predictions > 0.5).astype(np.uint8)
+
+            # Plota os resultados
+            self.plot_results(test_images, test_masks, predictions)
+        return self.model
+    
+    """
+    def predict(self, rgb_path: str, save_path: str) -> Model:
+        """
+        Segmenta imagem a partir de modelo.
+
+        Args:
+            rgb_path (str): Diretório contendo as imagens RGB.
+            save_path (str): Caminho para salvar resultado.
+        """
+        img = Image.open(rgb_path).convert('RGB')#.resize((img_size[1], img_size[0]))
+        img = np.array(img) / 255.0  # Normalizar para [0, 1]
+
+
+        # Predições no conjunto de teste
+        predictions = self.model.predict(test_images)
+        # Arredonda as segmentações para garantir que sejam binárias
+        predictions = (predictions > 0.5).astype(np.uint8)
+
+
+        return self.model
+    """
--- train_model.py
+++ train_model.py
@@ -1,124 +1,33 @@
-import tensorflow as tf
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.layers import Input, Conv2D, UpSampling2D, Concatenate
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
+
+import argparse
+import logging
 import numpy as np
-import cv2
-import os
-import matplotlib.pyplot as plt
 
-# Parâmetros
-IMG_WIDTH = 256
-IMG_HEIGHT = 256
-BATCH_SIZE = 8
-EPOCHS = 200
-DATASET_DIR = 'dataset'
-MASKS_DIR = 'masks'
-TEST_SPLIT = 0.1
+from net.resnet50 import SegmentationResnet50
+from config.config import IMG_WIDTH, IMG_HEIGHT, BATCH_SIZE, EPOCHS, TEST_SPLIT, VAL_SPLIT
 
-# Função para criar o modelo DeepLabV3+ com ResNet50 como backbone
-def deeplabv3_plus_resnet50(input_size=(IMG_HEIGHT, IMG_WIDTH, 3)):
-    base_model = ResNet50(weights='imagenet', include_top=False, input_shape=input_size)
     
-    # Extraindo camadas relevantes para o DeepLabV3+
-    x = base_model.output
-    
-    # Atrous Spatial Pyramid Pooling (ASPP)
-    b4 = Conv2D(256, (1, 1), padding='same', activation='relu')(x)
-    b4 = Conv2D(256, (3, 3), padding='same', activation='relu', dilation_rate=(6, 6))(b4)
-    
-    b6 = Conv2D(256, (1, 1), padding='same', activation='relu')(x)
-    b6 = Conv2D(256, (3, 3), padding='same', activation='relu', dilation_rate=(12, 12))(b6)
-    
-    b8 = Conv2D(256, (1, 1), padding='same', activation='relu')(x)
-    b8 = Conv2D(256, (3, 3), padding='same', activation='relu', dilation_rate=(18, 18))(b8)
-    
-    # Concatenar e aplicar uma convolução final
-    x = Concatenate()([x, b4, b6, b8])
-    x = Conv2D(256, (1, 1), padding='same', activation='relu')(x)
-    
-    # Adicionando camadas de upsampling para atingir a resolução desejada
-    x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(128, (3, 3), padding='same', activation='relu')(x)
-    x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(64, (3, 3), padding='same', activation='relu')(x)
-    x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(32, (3, 3), padding='same', activation='relu')(x)
-    x = UpSampling2D(size=(2, 2))(x)
-    x = Conv2D(16, (3, 3), padding='same', activation='relu')(x)
-    x = UpSampling2D(size=(2, 2))(x)  # UpSampling final
-    
-    # Camada de saída com a mesma dimensão das máscaras
-    x = Conv2D(1, (1, 1), padding='same', activation='sigmoid')(x)
-    
-    model = Model(inputs=base_model.input, outputs=x)
-    
-    # Compilar o modelo com BinaryIoU como métrica
-    model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=[tf.keras.metrics.BinaryIoU(threshold=0.5),'accuracy'])
-    
-    return model
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Treinamento de modelo DeepLabV3+ com ResNet50 para segmentação de vegetação.')
+    parser.add_argument('--rgb', required=True, help='Diretório contendo imagens RGB.')
+    parser.add_argument('--groundtruth', required=True, help='Diretório contendo máscaras binarizadas.')
+    parser.add_argument('--modelpath', required=True, help='Caminho para salvar o modelo final.')
 
-# Função para carregar e preprocessar as imagens
-def load_data(img_dir, mask_dir, img_size):
-    img_files = sorted([f for f in os.listdir(img_dir) if f.endswith('.png')])
-    mask_files = sorted([f for f in os.listdir(mask_dir) if f.endswith('.png')])
-    
-    images = []
-    masks = []
-    
-    for img_file, mask_file in zip(img_files, mask_files):
-        img = cv2.imread(os.path.join(img_dir, img_file))
-        img = cv2.resize(img, (img_size[1], img_size[0]))
-        img = img / 255.0  # Normalizar para [0, 1]
-        
-        mask = cv2.imread(os.path.join(mask_dir, mask_file), cv2.IMREAD_GRAYSCALE)
-        mask = cv2.resize(mask, (img_size[1], img_size[0]))
-        mask = np.expand_dims(mask, axis=-1)  # Adicionar uma dimensão para o canal
-        
-        images.append(img)
-        masks.append(mask)
-    
-    return np.array(images), np.array(masks)
+    args = parser.parse_args()
+    # Configuração do logging
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Carregar os dados
-images, masks = load_data(DATASET_DIR, MASKS_DIR, (IMG_HEIGHT, IMG_WIDTH))
+    resnet50 = SegmentationResnet50(
+        img_width= IMG_WIDTH,
+        img_height= IMG_HEIGHT,
+        batch_size= BATCH_SIZE,
+        epochs= EPOCHS,
+        validation_split= VAL_SPLIT,
+        test_split= TEST_SPLIT
+    )
 
-# Separar o conjunto de treino e teste
-split_idx = int(len(images) * (1 - TEST_SPLIT))
-train_images, test_images = images[:split_idx], images[split_idx:]
-train_masks, test_masks = masks[:split_idx], masks[split_idx:]
+    resnet50.train(dataset_dir= args.rgb, masks_dir= args.groundtruth)
+    resnet50.save_model(model_path=args.modelpath)
+    logging.info("Modelo treinado e salvo em: %s", args.modelpath)
 
-# Criar o modelo
-model = deeplabv3_plus_resnet50(input_size=(IMG_HEIGHT, IMG_WIDTH, 3))
 
-# Treinar o modelo
-model.fit(train_images, train_masks, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_split=0.1)
-
-# Prever no conjunto de teste
-predictions = model.predict(test_images)
-
-# Arredondar as previsões para garantir que sejam binárias
-predictions = (predictions > 0.5).astype(np.uint8)
-
-# Função para plotar os resultados
-def plot_results(images, masks, predictions, num=5):
-    fig, axes = plt.subplots(num, 3, figsize=(15, num * 5))
-    for i in range(num):
-        axes[i, 0].imshow(images[i])
-        axes[i, 0].set_title('Imagem Original')
-        axes[i, 0].axis('off')
-        
-        axes[i, 1].imshow(masks[i].squeeze(), cmap='gray')
-        axes[i, 1].set_title('Máscara Real')
-        axes[i, 1].axis('off')
-        
-        axes[i, 2].imshow(predictions[i].squeeze(), cmap='gray')
-        axes[i, 2].set_title('Previsão do Modelo')
-        axes[i, 2].axis('off')
-    
-    plt.tight_layout()
-    plt.show()
-
-# Plotar os resultados para algumas amostras do conjunto de teste
-plot_results(test_images, test_masks, predictions)
