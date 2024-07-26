6c144a76cfe4fa233ec8c53dcaba6af47ff9a4df
José Pedro Belo <josepedroribeirobelo@gmail.com>
2024-07-26 13:37:54 -0300

    Ajustando parametros de configuração

--- config/config.py
+++ config/config.py
@@ -5,6 +5,15 @@
 SUBTILES_SIZE = (256,256)
 
 #Limiar para binarizar máscara de segmentação
-BINARIZE_THRESHOLD = 128    
+BINARIZE_THRESHOLD = 75
+
 #Valor do pixel da classe segunda classe binária (vegetação)
 BINARY_MAX = 1
+
+#Treinamento 
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+BATCH_SIZE = 32
+EPOCHS = 200
+TEST_SPLIT = 0
+VAL_SPLIT = 0.15
